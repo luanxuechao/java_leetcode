a38e874e5867615eb3235cafd1fa9e9d625c27f7
luanx <xuechao.luan@autodesk.com>
2020-07-08 11:09:18 +0800

    daily commit

---
+++ src/com/leetcode/easy/BalancedBinaryTree110.java
@@ -0,0 +1,24 @@
+package com.leetcode.easy;
+
+public class BalancedBinaryTree110 {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        int leftDepth = getDepth(root.left);
+        int rightDepth = getDepth(root.right);
+        if (Math.abs(leftDepth - rightDepth) > 1) {
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int getDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = getDepth(root.left);
+        int rightDepth = getDepth(root.right);
+        return Math.max(leftDepth, rightDepth);
+    }
+}
