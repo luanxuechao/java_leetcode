e0977d5399101314771709067878c7faf2250e87
luanx <xuechao.luan@autodesk.com>
2020-07-17 15:22:13 +0800

    daily commit

---
+++ src/com/leetcode/medium/NextPermutation31.java
@@ -0,0 +1,35 @@
+package com.leetcode.medium;
+
+public class NextPermutation31 {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i < 0) {
+            reverse(nums, 0);
+            return;
+        }
+        int j = nums.length - 1;
+        while (j >= 0 && nums[j] <= nums[i]) {
+            j--;
+        }
+        swap(nums, i, j);
+        reverse(nums, i + 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
+    }
+
+    private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+}
