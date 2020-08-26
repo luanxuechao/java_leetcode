ca915a8d1544596c8d53d62341c20484ef67927d
luanx <xuechao.luan@autodesk.com>
2020-08-26 17:51:42 +0800

    daily commit

--- src/com/leetcode/medium/SetMatrixZeroes73.java
+++ src/com/leetcode/medium/SetMatrixZeroes73.java
@@ -1,7 +1,28 @@
 package com.leetcode.medium;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetMatrixZeroes73 {
     public void setZeroes(int[][] matrix) {
-
+        int rLen = matrix.length;
+        int cLen = matrix.length;
+        Set<Integer> rows = new HashSet<>();
+        Set<Integer> cols = new HashSet<>();
+        for (int i = 0; i < rLen; i++) {
+            for (int j = 0; j < cLen; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    cols.add(j);
+                }
+            }
+        }
+        for (int i = 0; i < rLen; i++) {
+            for (int j = 0; j < cLen; j++) {
+                if (rows.contains(i) || cols.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
 }
